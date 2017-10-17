c0c97fb9c5c81c5a728e984f4b58fc62176e5b13
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2017-10-17 01:30:18 -0400

    Add OTFByteAllocator#allocate tests
    I'm still not entirely sure about everything going on in this, but
    figure that the byte allocators seem to have the most logic and it might
    make sense to start adding tests around said logic.
    
    I didn't add any tests for the allocate method with the data parameter
    because I'm not sure I understand the allocMap's purpose in the file
    yet.

---
+++ src/test/scala/com/tsunderebug/scolortesting/otf/OTFByteAllocatorTest.scala
@@ -0,0 +1,40 @@
+package com.tsunderebug.scolor.otf
+
+import com.tsunderebug.scolor.Offset
+import com.tsunderebug.scolor.otf.types.OTFOffset32
+
+import spire.math.{UByte, UInt}
+
+import org.scalatest.{FlatSpec, PrivateMethodTester}
+
+class OTFByteAllocatorTest extends FlatSpec with PrivateMethodTester {
+  
+  /* use PrivateMethodTester to access private values in class */
+  val nextAvailableOffset = PrivateMethod[Offset]('nextAvailableOffset)
+
+  "allocate" should "move the next available offset correctly" in {
+    val otfByteAllocator = new OTFByteAllocator(OpenTypeFont(Nil))    
+    otfByteAllocator.allocate(UInt(10))
+
+    val nextOffset = otfByteAllocator invokePrivate nextAvailableOffset()
+    assertResult(UInt(10))(nextOffset.position)
+  }
+
+  it should "return an offset describing the next open space these bytes can fit" in {
+    val otfByteAllocator = new OTFByteAllocator(OpenTypeFont(Nil))    
+    
+    /* first we expect 0 */
+    assertResult(OTFOffset32(0)) {
+      otfByteAllocator.allocate(UInt(10))
+    }
+
+    /* but after we allocate 10 above, we'll expect to be told we can allocate our
+     * next set of bytes to the 10th position (which is 1 higher than our previous
+     * allocation since we're 0-based)
+     */
+    assertResult(OTFOffset32(10)) {
+      otfByteAllocator.allocate(UInt(6))
+    }
+  }
+  
+}
