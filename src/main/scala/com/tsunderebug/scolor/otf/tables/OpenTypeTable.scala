e6803b84a1688dfe430380209570d719c3fdbdfa
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2017-10-17 00:33:16 -0400

    Fix indentation in OpenTypeTable

--- src/main/scala/com/tsunderebug/scolor/otf/tables/OpenTypeTable.scala
+++ src/main/scala/com/tsunderebug/scolor/otf/tables/OpenTypeTable.scala
@@ -7,9 +7,9 @@ import spire.math.UByte
 abstract class OpenTypeTable extends Table {
 
   override def getBytes(b: ByteAllocator): Array[UByte] = {
-  	sections(b).foldLeft(Array.empty[UByte]) {
-  		case (accum, section) => accum ++ section.getBytes(b)
-  	}
+    sections(b).foldLeft(Array.empty[UByte]) {
+      case (accum, section) => accum ++ section.getBytes(b)
+    }
   }
   
   def getPosition(b: ByteAllocator): Offset = b.allocate(this)
